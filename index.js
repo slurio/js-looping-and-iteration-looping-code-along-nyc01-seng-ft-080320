45590619afe5da9e8e4ccbe3ba0b0a1669907586
slurio <samantha.lurio@gmail.com>
2020-09-12 17:22:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let message = []
+    for (let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return message
+}
+
+function countDown (num) {
+    let i = 0;
+    while(i < num + 1) {
+        console.log(num - i);
+        i ++;
+    }
+}
